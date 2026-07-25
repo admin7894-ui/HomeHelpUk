38beea1b598e2ae6b8e14284e77faad7b8ea39cf
UK Project <tasmiha4valentiz@gmail.com>
2026-07-25 10:23:17 +0530

    Move backend to server directory

--- routes/categories.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const categoriesController = require('../controllers/categoriesController');
-
-router.get('/', categoriesController.getAll);
-router.get('/services/:serviceId', categoriesController.getServiceDetail);
-router.get('/:id', categoriesController.getById);
-
-module.exports = router;
--- tests/verify_multi_model_allowed.test.js
+++
@@ -1,9 +0,0 @@
-const { isPricingModelAllowed } = require('../utils/pricingModelScoping');
-
-console.log('=== Testing isPricingModelAllowed for multi model ===');
-
-const isAllowed = isPricingModelAllowed('service_home_cook', 'Cooking', 'multi', ['per_hour', 'per_person']);
-console.log('isPricingModelAllowed result:', isAllowed);
-console.assert(isAllowed === true, 'multi model with per_hour and per_person must be allowed!');
-
-console.log('✅ IS PRICING MODEL ALLOWED UNIT TEST PASSED 100% CLEAN!');
